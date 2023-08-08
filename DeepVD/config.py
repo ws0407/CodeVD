89f33a5203a8eb5879c3e557fffee9f6d17f0c06
unknown <2395967268@qq.com>
2023-08-08 16:26:41 +0800

    initial project

---
+++ DeepVD/config.py
@@ -0,0 +1,3 @@
+INPUT_PATH = "/data/data/ws/CodeVD/DeepVD/data/BigVul_data"
+OUTPUT_PATH = "/data/data/ws/CodeVD/DeepVD/data/output"
+RAND_DATA_PATH = "/data/data/ws/CodeVD/DeepVD/data/random_data"
---
+++ README.md
@@ -0,0 +1,1 @@
+## A collection of Vulnerabilities Detection from Source Code
